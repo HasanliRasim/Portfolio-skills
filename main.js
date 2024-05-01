8aee85f4109513accf418db9a8224c37ead86d46
HasanliRasim <https://github.com/HasanliRasim>
2024-05-02 00:11:03 +0300

    Skills

---
+++ main.js
@@ -0,0 +1,12 @@
+const skills1 = document.querySelector("#skillHtml")
+setTimeout(() => {
+  skills1.style.width = "90%"
+})
+const skills2 = document.querySelector("#skillSass")
+setTimeout(() => {
+  skills2.style.width = "70%"
+})
+const skills3 = document.querySelector("#skillsJs")
+setTimeout(() => {
+  skills3.style.width = "30%"
+})
